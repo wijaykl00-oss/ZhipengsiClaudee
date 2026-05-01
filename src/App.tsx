6c991b03688c04a5e87cde6fe0c1ed402bf92cdd
wijayy <wijayawa@gmail.com>
2026-05-01 22:58:03 +0700

    Update QRIS image to qrisjyy.jpeg

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ import claudeImg from '../foto/claudes.jpeg';
 import cursorImg from '../foto/cursor.jpg';
 import gptImg from '../foto/gpt.png';
 import backgroundImg from '../foto/Background.png';
-import qrisImg from '../foto/qrisjy.jpeg';
+import qrisImg from '../foto/qrisjyy.jpeg';
 
 const products = [
   {
