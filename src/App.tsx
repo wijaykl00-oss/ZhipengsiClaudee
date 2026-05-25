004c024dd371af1d90367180f77ba4df2c521f87
wijayy <wijayawa@gmail.com>
2026-05-25 11:34:11 +0700

    fix: ganti gambar QRIS dari Zangxhi.jpeg ke qrisbb.jpeg

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ import claudeImg from '../foto/claudes.jpeg';
 import cursorImg from '../foto/cursor.jpg';
 import gptImg from '../foto/gpt.png';
 import backgroundImg from '../foto/Background.png';
-import qrisImg from '../foto/Zangxhi.jpeg';
+import qrisImg from '../foto/qrisbb.jpeg';
 import backgroundpbImg from '../foto/backgroundpb.jpeg';
 
 const products = [
