b210f44fe5240436d85e92bdcfae63840db2b5a5
wijayy <wijayawa@gmail.com>
2026-06-17 05:25:28 +0700

    emmm

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ import claudeImg from '../foto/claudes.jpeg';
 import cursorImg from '../foto/cursor.jpg';
 import gptImg from '../foto/gpt.png';
 import backgroundVideo from '../foto/Background1.mp4';
-import qrisImg from '../foto/qrlgi.jpeg';
+import qrisImg from '../foto/qrtjtu.jpeg';
 import backgroundpbImg from '../foto/backgroundpb.jpeg';
 import productBackgroundVideo from '../foto/Background2.mp4';
 
